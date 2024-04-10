2bfbdb7492e8e64fbb9234c10cb62246b6a0e8ba
Karmendes <lucaseqt@gmail.com>
2024-04-10 21:24:48 +0100

    infra: changing dtypes from column sentido

--- infra/db/init.sql
+++ infra/db/init.sql
@@ -4,27 +4,27 @@ CREATE SCHEMA IF NOT EXISTS sc_gold;
 
 -- create the table silver
 CREATE TABLE sc_silver.tb_brt_api (
-  codigo VARCHAR(2048),
-  placa VARCHAR(6),
-  linha VARCHAR(6),
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  codigo VARCHAR(255), -- Adjust data type based on expected code length
+  placa VARCHAR,
+  linha VARCHAR,
   latitude float,
   longitude float,
-  dataHora int,
+  dataHora TIMESTAMP,  -- Use a proper date/time data type
   velocidade float,
   id_migracao_trajeto VARCHAR,
   sentido VARCHAR,
   trajeto VARCHAR,
-  hodometro float,
-  direcao int,
-  dh_extraction datetime
+  hodometro FLOAT,
+  direcao VARCHAR,
+  dh_extraction TIMESTAMP
 );
 
--- create the gold
-CREATE TABLE sc_silver.tb_brt_api (
-  codigo VARCHAR(2048),
-  latitude float,
-  longitude float,
-  velocidade float,
+-- create the gold table
+CREATE TABLE sc_gold.tb_brt_api (
+  codigo VARCHAR(255), -- Adjust data type based on expected code length
+  latitude FLOAT,
+  longitude FLOAT,
+  velocidade FLOAT
 );
-
 
